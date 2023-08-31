be5041c3e6f0be9932a44354daeb8eba70aad666
Andrea-Cal <andiesc83@hotmail.com>
2023-08-31 17:35:39 -0300

    agrego icono de corazon en las cards, efectos en el hover de las cards y cambio el color de los botones. Renombro los archivos past.js y upcoming.js

--- js/main_index.js
+++ js/main_index.js
@@ -1,6 +1,5 @@
 // carousel
 const carouselHome = document.getElementById('carousel-home');
-
 let templateCarousel = '';
 
 for (let evento of data.events) {
@@ -11,23 +10,25 @@ for (let evento of data.events) {
     </div>`
   }  
 };
-
 carouselHome.innerHTML = templateCarousel;
 
 
 // cards
 const cardsHome = document.getElementById('card-section-home');
-
 let templateCards = '';
 
 for (let evento of data.events) {
   templateCards += `<div class="card shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
   <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+  </svg>
   <div class="card-body d-flex flex-column justify-content-between">
     <h5 class="card-title">${evento.name}</h5>
     <p class="card-text">${evento.description}</p>
     <div class="d-flex justify-content-between align-items-center">
-      <h6 class="card-title mb-0">$ ${evento.price}</h6>
+      <h5 class="card-title mb-0">$ ${evento.price}</h5>
       <a href="#" class="btn btn-primary">Details</a>
     </div>          
   </div>
@@ -37,11 +38,8 @@ for (let evento of data.events) {
 cardsHome.innerHTML = templateCards;
 
 // checkbox categorias
-
 const checkboxCategorias = document.getElementById('checkbox-categorias');
-
 let templateCategorias = '';
-
 let listadoDeCategorias = [];
 
 for (let evento of data.events) {
@@ -49,7 +47,6 @@ for (let evento of data.events) {
     listadoDeCategorias.push(evento.category)    
   }  
 };
-
 for (let categoria of listadoDeCategorias) {
   templateCategorias += `<label><input type="checkbox" id="cbox${categoria.indexOf}" name="cbox" value="${categoria}"/> ${categoria}</label>`
 }
--- js/upcoming.js
+++ js/upcoming.js
@@ -16,14 +16,18 @@ carouselUpcoming.innerHTML = templateCarousel;
 const cardsUpcoming = document.getElementById('card-section-upcoming');
 let templateCards = '';
 for (let evento of data.events) {
-  if(data.currentDate < evento.date){
+  if(data.currentDate <= evento.date){
     templateCards += `<div class="card shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
       <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+      </svg>
       <div class="card-body d-flex flex-column justify-content-between">
         <h5 class="card-title">${evento.name}</h5>
         <p class="card-text">${evento.description}</p>
         <div class="d-flex justify-content-between align-items-center">
-          <h6 class="card-title mb-0">$ ${evento.price}</h6>
+          <h5 class="card-title mb-0">$ ${evento.price}</h5>
           <a href="#" class="btn btn-primary">Details</a>
         </div>          
       </div>
