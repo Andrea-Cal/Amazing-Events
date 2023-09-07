df84d730a9f85ad48fd5caac1ada5a1a9fc35bc5
Andrea-Cal <andiesc83@hotmail.com>
2023-09-07 20:57:26 -0300

    ultimo commit antes de crear nueva rama

--- js/past.js
+++ js/past.js
@@ -104,7 +104,7 @@ function crearEstructuraCard(objetoEvento){
   let template = '';  
   template += `
   <div class="card shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
-    <img src="${objetoEvento.image}" class="card-img-top" alt="${objetoEvento.name}">      
+    <img src="${objetoEvento.image}" class="card-img-top" alt="${objetoEvento.name}">          
     <div class="card-body d-flex flex-column justify-content-between">
       <h5 class="card-title">${objetoEvento.name}</h5>
       <p class="card-text">${objetoEvento.description}</p>
@@ -131,6 +131,7 @@ function imprimirCardsEnHtml(arrayDeEventos, elementoHtml){
 }
 imprimirCardsEnHtml(arrayDeEventosPasados, cardsPast);
 
+// funcion que crea la estructura del mensaje
 function crearEstructuraMensaje(){
   let template = `
     <div class="card text-center">
@@ -146,16 +147,13 @@ function crearEstructuraMensaje(){
   return template;
 }
 
+// funcion que imprime el mensaje cuando los resultados de la busqueda no coinciden
 function imprimirMensajeBusquedaNoCoincide(elementoHtml){ 
   let template = crearEstructuraMensaje();
   elementoHtml.innerHTML = template;
 }
 
-// corazones
-/* <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-</svg> */
+
 
 
 
