4f583ca671070149605ed529f137deb980dca447
Andrea-Cal <andiesc83@hotmail.com>
2023-09-09 02:33:38 -0300

    agrego funciones para obtener los datos de la tabla1 en archivo stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,9 +1,21 @@
 fetch("https://mindhub-xj03.onrender.com/api/amazing")
 .then( (response) => response.json())
 .then (data => {  
+  let objetoData = data;
   let arrayEventos = data.events;
+  console.log(arrayEventos);
   // llamado a la funcion que muestra imagenes en el carousel
-  mostrarImagenesEnCarousel(arrayEventos, carouselStats);  
+  mostrarImagenesEnCarousel(arrayEventos, carouselStats);
+  // filtramos los eventos pasados
+  let arrayDeEventosPasados = arrayEventos.filter(objetoEvento => objetoData.currentDate > objetoEvento.date);
+  // filtramos los eventos futuros
+  let arrayDeEventosFuturos = arrayEventos.filter(objetoEvento => objetoData.currentDate <= objetoEvento.date);
+  // evento con mayor porcentaje de asistencia - tabla 1.1
+  eventoConMayorAsistencia(arrayDeEventosPasados);
+  // evento con menor porcentaje de asistencia - tabla 1.2
+  eventoConMenorAsistencia(arrayDeEventosPasados);
+  // evento con mayor capacidad - tabla 1.3
+  eventoConMayorCapacidad(arrayEventos);
 })
 .catch( error => { console.log(error);})
 
@@ -28,3 +40,34 @@ function mostrarImagenesEnCarousel(array, elementoHtml){
   }
   elementoHtml.innerHTML = templateCarousel;
 }
+
+
+// ------------------------------------------------------------
+// evento con mayor porcentaje de asistencia - tabla 1.1
+function eventoConMayorAsistencia(array){
+  let eventosOrdenadosDeMayorAMenor = array.sort((a,b) => b.assistance - a.assistance);
+  let mayor = eventosOrdenadosDeMayorAMenor[0];
+  return mayor;
+}
+// insertar el dato en la celda correspondiente
+
+
+// ------------------------------------------------------------
+// evento con menor porcentaje de asistencia - tabla 1.2
+function eventoConMenorAsistencia(array){
+  let eventosOrdenadosDeMenorAMayor = array.sort((a,b) => a.assistance - b.assistance);
+  let menor = eventosOrdenadosDeMenorAMayor[0];
+  return menor;
+}
+// insertar el dato en la celda correspondiente
+
+
+// ------------------------------------------------------------
+// evento con menor capacidad - tabla 1.3
+function eventoConMayorCapacidad(array){
+  let eventosOrdenadosDeMayorAMenor = array.sort((a,b) => b.capacity - a.capacity);
+  let mayor = eventosOrdenadosDeMayorAMenor[0];
+  return mayor;
+}
+// insertar el dato en la celda correspondiente
+// ------------------------------------------------------------
