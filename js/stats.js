1c5f1fffbfa29f3e3faa5a77f71c0ec3787c258e
Andrea-Cal <andiesc83@hotmail.com>
2023-09-09 21:13:35 -0300

    agrego funciones para generar los datos de la tabla2

--- js/stats.js
+++ js/stats.js
@@ -15,20 +15,26 @@ fetch("https://mindhub-xj03.onrender.com/api/amazing")
   // evento con menor porcentaje de asistencia - tabla 1.2
   eventoConMenorAsistencia(arrayDeEventosPasados, tabla1Dato2);
   // evento con mayor capacidad - tabla 1.3
- console.log( eventoConMayorCapacidad(arrayEventos, tabla1Dato3));
+  eventoConMayorCapacidad(arrayEventos, tabla1Dato3);
+  const categorias = [ ...new Set(arrayDeEventosFuturos.map(objeto => objeto.category))];
+  // por cada categoria
+  generarDatos(categorias, arrayDeEventosFuturos, tabla2); 
 })
 .catch( error => { console.log(error);})
 
 // contenedor carousel
 const carouselStats = document.getElementById('carousel-stats');
+// celdas de la tabla 1
 const tabla1Dato1 = document.getElementById('td-tabla1-1');
 const tabla1Dato2 = document.getElementById('td-tabla1-2');
 const tabla1Dato3 = document.getElementById('td-tabla1-3');
+// contenedor tabla 2
+const tabla2 = document.getElementById('tabla2');
 
-// mostrar imagenes de los primeros 5 eventos en el carousel
+// mostrar imagenes de los primeros 3 eventos en el carousel
 function mostrarImagenesEnCarousel(array, elementoHtml){
   let templateCarousel = '';
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 3; i++) {
     if(i === 0){
       templateCarousel += `
       <div class="carousel-item active">
@@ -67,4 +73,48 @@ function eventoConMayorCapacidad(array, elementoHtml){
 }
 // ------------------------------------------------------------
 
+// crear estructura de tabla (tabla2)
+function crearEstructuraTabla2(categoria, recaudacion, porcentaje){
+  let template = '';
+  template += `
+    <tr>
+      <td class="col-4 border border-secondary-subtle">${categoria}</td>
+      <td class="col-4 border border-secondary-subtle">${recaudacion}</td>
+      <td class="col-4 border border-secondary-subtle">${porcentaje}</td>
+    </tr>
+  `
+  return template;
+}
+
+// eventos futuros
+//let eventosFuturos = data.events.filter(objetoEvento => data.currentDate <= objetoEvento.date);
+// array de categorias de eventos futuros
+//const categorias = [ ...new Set(eventosFuturos.map(objeto => objeto.category))];
+
+// por cada categoria 
+function generarDatos(arrayDeCategorias, arrayObjetos, elementoHtml){
+  let estructura = "";
+  arrayDeCategorias.forEach(categoria => { 
+    let eventosFiltrados = arrayObjetos.filter(objetoEvento => objetoEvento.category.includes(categoria));
+    let recaudacion = calcularRecaudacionPorCategoria(eventosFiltrados);
+    let porcentaje = calcularPorcentajeDeAsistencia(eventosFiltrados);
+    estructura += crearEstructuraTabla2(categoria, recaudacion, porcentaje);
+  });
+  elementoHtml.innerHTML = estructura;
+}
+
+// funcion que calcula el total de ingresos por categoria
+function calcularRecaudacionPorCategoria(array){
+  let recaudacion = 0;
+  array.forEach(element => { recaudacion += (element.estimate*element.price) });    
+  return recaudacion;
+}
+
+// funcion que calcula el porcentaje de asistencia estimada de todos los eventos de una misma categoria
+function calcularPorcentajeDeAsistencia(array){  
+  let suma = 0;
+  array.forEach(element => { suma += ((element.estimate*100)/element.capacity) });  
+  let porcentajeAsistencia = (suma/array.length).toFixed(2);  
+  return porcentajeAsistencia;
+}
 
